a4e7c4a50f7dc9d9c922286a4fae9c211a9d80da
favo004 <jfavo@hotmail.com>
2021-03-12 11:59:42 -0500

    Added test for base64 conversion

--- TiledReaderTest/TiledReaderTests.cs
+++ TiledReaderTest/TiledReaderTests.cs
@@ -7,11 +7,19 @@ namespace TiledReaderTest
     public class TiledReaderTests
     {
         [Fact]
-        public void TiledReaderReadsInFile()
+        public void TiledReaderCSVFile()
         {
             string testFilePath = @"C:\Users\JFavo\source\repos\TiledReader\TiledReaderTest\TestData\CSVTestMap.tmx";
 
             var data = TiledMapReader.GenerateMap(testFilePath);
+        }
+
+        [Fact]
+        public void TiledReaderBase64File()
+        {
+            string path = @"C:\Users\JFavo\source\repos\TiledReader\TiledReaderTest\TestData\Base64TestMap.tmx";
+
+            var data = TiledMapReader.GenerateMap(path);
         }
     }
 }
