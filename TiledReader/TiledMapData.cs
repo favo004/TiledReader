44279bd3f0518be8ee0b7aa77c202ccb710042e3
favo004 <jfavo@hotmail.com>
2021-03-14 11:17:07 -0400

    Added Zlib and GZip decompression methods.

--- TiledReader/TiledMapData.cs
+++ TiledReader/TiledMapData.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using ZstdNet;
 
@@ -144,7 +147,17 @@ namespace TiledReader
         }
         private void ZLibDecompression()
         {
+            byte[] data = Convert.FromBase64String(RawTileData);
+            var outputStream = new MemoryStream();
 
+            using(var compressedStream = new MemoryStream(data))
+            using(var inputStream = new InflaterInputStream(compressedStream))
+            {
+                inputStream.CopyTo(outputStream);
+                ConvertRawDataFromByteArray(outputStream.ToArray());
+            }
+
+            outputStream.Dispose();
         }
         private void StandardBase64()
         {
@@ -153,7 +166,17 @@ namespace TiledReader
         }
         private void GZipDecompression()
         {
+            byte[] data = Convert.FromBase64String(RawTileData);
+            var outputStream = new MemoryStream();
 
+            using (var compressedStream = new MemoryStream(data))
+            using (var inputStream = new GZipInputStream(compressedStream))
+            {
+                inputStream.CopyTo(outputStream);
+                ConvertRawDataFromByteArray(outputStream.ToArray());
+            }
+
+            outputStream.Dispose();
         }
         private void XMLDecompression()
         {
